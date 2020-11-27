ae10f9a36a1f1546bfe8145e98d7d9476d0e5f01
Piotr <zdun.piotr@gmail.com>
2020-11-28 00:15:26 +0100

    Add intente

---
+++ app/src/main/java/coddiers/hackyeah/dziki/ui/dashboard/MapToApplicationActivity.kt
@@ -0,0 +1,12 @@
+package coddiers.hackyeah.dziki.ui.dashboard
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import coddiers.hackyeah.dziki.R
+
+class MapToApplicationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_map_to_application)
+    }
+}
