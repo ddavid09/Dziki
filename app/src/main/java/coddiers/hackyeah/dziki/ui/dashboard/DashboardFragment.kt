4b4854c567aef35a8fd362420279c33a7edf9cd2
Joberooo <patryk.barczak@student.wat.edu.pl>
2020-11-27 23:37:50 +0100

    Add dashboard

--- app/src/main/java/coddiers/hackyeah/dziki/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/coddiers/hackyeah/dziki/ui/dashboard/DashboardFragment.kt
@@ -1,31 +1,70 @@
 package coddiers.hackyeah.dziki.ui.dashboard
 
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.location.Address
+import android.location.Geocoder
 import android.os.Bundle
+import android.view.KeyEvent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.EditText
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
+import androidx.fragment.app.FragmentActivity
 import coddiers.hackyeah.dziki.R
 
-class DashboardFragment : Fragment() {
 
-    private lateinit var dashboardViewModel: DashboardViewModel
+class DashboardFragment : Fragment() {
+    private lateinit var cityEditText: EditText;
+    private lateinit var boroughEditText: EditText;
+    private lateinit var voivodeshipEditText: EditText;
+    private lateinit var goToMapButton: Button;
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        dashboardViewModel =
-            ViewModelProvider(this).get(DashboardViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_dashboard, container, false)
-        val textView: TextView = root.findViewById(R.id.text_dashboard)
-        dashboardViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
+        cityEditText = root.findViewById(R.id.city_text_view)
+        boroughEditText = root.findViewById(R.id.borough_text_view)
+        voivodeshipEditText = root.findViewById(R.id.voivodeship_text_view)
+        goToMapButton = root.findViewById(R.id.go_to_map_button)
+        cityEditText.setOnKeyListener(View.OnKeyListener { _, keyCode, keyevent ->
+            if (keyCode == KeyEvent.KEYCODE_ENTER && keyevent.action == KeyEvent.ACTION_UP) {
+                getAddress(cityEditText.text.toString())
+                hideKeyboard(this.activity)
+                return@OnKeyListener true
+            }
+            false
         })
         return root
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun getAddress(name: String) {
+        val coder = Geocoder(context)
+        val address: List<Address>?
+
+        try {
+            address = coder.getFromLocationName(name, 5)
+            val location: Address = address[0]
+            boroughEditText.setText("" + location.subAdminArea.toString())
+            voivodeshipEditText.setText("" + location.adminArea.toString())
+        } catch (ex: Exception) {
+            ex.printStackTrace()
+        }
+    }
+
+    private fun hideKeyboard(activity: FragmentActivity?) {
+        val imm = activity?.getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
+        var view = activity.currentFocus
+        if (view == null) {
+            view = View(activity)
+        }
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
     }
 }
