c7f920ac6ab07b6421b7c9d0e495a838295c5097
Piotr <zdun.piotr@gmail.com>
2020-11-28 02:20:21 +0100

    Add activity

---
+++ app/src/main/java/coddiers/hackyeah/dziki/BoarNotificationAvtivity.kt
@@ -0,0 +1,11 @@
+package coddiers.hackyeah.dziki
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BoarNotificationAvtivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_boar_notification_avtivity)
+    }
+}
