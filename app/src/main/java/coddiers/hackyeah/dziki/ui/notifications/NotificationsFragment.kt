d0a86faf933f0aeef0f05422376d2fafbf7ac2e6
h4dri <adrianmalik333@gmail.com>
2020-11-28 03:26:24 +0100

    stop working start drinking

--- app/src/main/java/coddiers/hackyeah/dziki/ui/notifications/NotificationsFragment.kt
+++ app/src/main/java/coddiers/hackyeah/dziki/ui/notifications/NotificationsFragment.kt
@@ -2,13 +2,17 @@ package coddiers.hackyeah.dziki.ui.notifications
 
 import android.app.Activity
 import android.os.Bundle
+import android.provider.ContactsContract
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.*
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import coddiers.hackyeah.dziki.R
 import coddiers.hackyeah.dziki.database.DataBase
+import coddiers.hackyeah.dziki.database.Report
 import kotlinx.android.synthetic.main.fragment_notifications.*
 
 
@@ -106,7 +110,34 @@ class NotificationsFragment : Fragment() {
                 2 -> dead = true
             }
 
-            DataBase().getReports(dead, voivodeship, district, "")
+            val notifications = DataBase().getReports(dead, voivodeship, district, "")
+            var idList: ArrayList<String> = ArrayList()
+            var descriptionList: ArrayList<String> = ArrayList()
+            notifications.observe(viewLifecycleOwner, Observer { reportsList ->
+                if(reportsList!=null){
+                    idList = ArrayList()
+                    descriptionList = ArrayList()
+                    for( report in reportsList){
+                        idList.add(report.ID);
+                        descriptionList.add(report.description)
+                    }
+                    val idArrayAdapter: ArrayAdapter<String> = ArrayAdapter(requireContext(),
+                    android.R.layout.simple_dropdown_item_1line, idList)
+                    photosList.adapter = idArrayAdapter
+
+                    val descriptionArrayAdapter: ArrayAdapter<String> = ArrayAdapter(requireContext(),
+                    android.R.layout.simple_dropdown_item_1line, descriptionList)
+                    notificationList.adapter = descriptionArrayAdapter
+
+                    Log.d("Adapter", photosList.toString())
+                    Log.d("Adapter", notificationList.toString())
+                }else{
+                    Log.d("Adapter", "ELSE")
+                }
+            })
+
+
+
         }
     }
 
