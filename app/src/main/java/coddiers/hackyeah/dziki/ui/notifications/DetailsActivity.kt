bac1f9ff8d73e2d461f488128d1565d8a71d587a
h4dri <adrianmalik333@gmail.com>
2020-11-28 01:04:10 +0100

    Add notifications2

--- app/src/main/java/coddiers/hackyeah/dziki/ui/notifications/DetailsActivity.kt
+++ app/src/main/java/coddiers/hackyeah/dziki/ui/notifications/DetailsActivity.kt
@@ -2,6 +2,8 @@ package coddiers.hackyeah.dziki.ui.notifications
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import coddiers.hackyeah.dziki.R
+import kotlinx.android.synthetic.main.activity_details.*
 
 class DetailsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/coddiers/hackyeah/dziki/ui/notifications/NotificationsFragment.kt
+++ app/src/main/java/coddiers/hackyeah/dziki/ui/notifications/NotificationsFragment.kt
@@ -1,31 +1,59 @@
 package coddiers.hackyeah.dziki.ui.notifications
 
+import android.app.Activity
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.*
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
 import coddiers.hackyeah.dziki.R
+import kotlinx.android.synthetic.main.fragment_notifications.*
+
 
 class NotificationsFragment : Fragment() {
 
-    private lateinit var notificationsViewModel: NotificationsViewModel
+    val types = arrayOf("simple User", "Admin")
+
+    lateinit var oprion : Spinner
+    lateinit var result : TextView
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        notificationsViewModel =
-            ViewModelProvider(this).get(NotificationsViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_notifications, container, false)
-        val textView: TextView = root.findViewById(R.id.text_notifications)
-        notificationsViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
         return root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val voivodeships = resources.getStringArray(R.array.voivodeships)
+
+        val arrayAdapter = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, voivodeships)
+        voivodeshipSpinner.adapter = arrayAdapter
+
+        voivodeshipSpinner.onItemSelectedListener = object :
+            AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                button2.text = voivodeships[position]
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                TODO("Not yet implemented")
+            }
+
+
+        }
+    }
+
+
+
 }
