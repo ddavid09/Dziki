e0fb239aea693c41897e9f66a3ec69e1233f49a1
h4dri <adrianmalik333@gmail.com>
2020-11-28 01:04:00 +0100

    Add Notifications

---
+++ app/src/main/java/coddiers/hackyeah/dziki/ui/notifications/DetailsActivity.kt
@@ -0,0 +1,11 @@
+package coddiers.hackyeah.dziki.ui.notifications
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_details)
+    }
+}
